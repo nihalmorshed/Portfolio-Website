7f321798e5bd44ae0cf3f18c789d20dad416a701
Nihal Morshed <nilumorshed@gmail.com>
2025-01-27 23:10:12 +0600

    Add contact form styles and input animation functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -115,6 +115,25 @@ var swiper = new Swiper(".testimonials__container", {
 
 /*=============== INPUT ANIMATION ===============*/
 
+const inputs = document.querySelectorAll('.input');
+
+function focusFunc() {
+    let parent = this.parentNode;
+    parent.classList.add('focus');
+}
+
+function blurFunc() {
+    let parent = this.parentNode;
+    if (this.value == "") {
+        parent.classList.remove('focus');
+    }
+}
+
+inputs.forEach(input => {
+    input.addEventListener('focus', focusFunc);
+    input.addEventListener('blur', blurFunc);
+});
+
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
