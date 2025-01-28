17ea44c57617118711706c5a9a833323ddc25d60
Nihal Morshed <nilumorshed@gmail.com>
2025-01-29 00:48:45 +0600

    Implement sidebar toggle functionality and active link highlighting on scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,25 @@
 /*=============== SHOW SIDEBAR ===============*/
-
+const navMenu = document.getElementById('sidebar'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
 /*===== SIDEBAR SHOW =====*/
 /* Validate If Constant Exists */
+if(navToggle) {
+	navToggle.addEventListener("click", () => {
+		navMenu.classList.add('show-sidebar')
+	})
+}
+
 
 
 /*===== SIDEBAR HIDDEN =====*/
 /* Validate If Constant Exists */
+if(navClose) {
+	navClose.addEventListener("click", () => {
+		navMenu.classList.remove('show-sidebar')
+	})
+}
 
 
 /*=============== SKILLS TABS ===============*/
@@ -136,6 +149,27 @@ inputs.forEach(input => {
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
 
+window.addEventListener("scroll", navHighlighter);
+
+
+function navHighlighter()
+{
+	let scrollY = window.pageYOffset;
+	sections.forEach(current => {
+		const sectionHeight = current.offsetHeight;
+		const sectionTop = current.offsetTop - 50,
+		sectionID = current.getAttribute("id");
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight)
+		{
+			document.querySelector('.nav__menu a[href*=' + sectionID + ']').classList.add("active-link")
+		}
+		else
+		{
+			document.querySelector('.nav__menu a[href*=' + sectionID + ']').classList.remove("active-link")
+		 }
+	})
+}
 
 /*=============== SHOW SCROLL UP ===============*/
